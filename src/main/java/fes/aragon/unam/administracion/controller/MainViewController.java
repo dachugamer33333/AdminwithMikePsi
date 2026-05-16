7dca7061c1193b5515f8e017651497df194f1c6a
dachu_dev <dachu_dev@MacBook-Air-de-dachu-dev.local>
2026-05-16 11:20:29 -0600

    v1

--- src/main/java/fes/aragon/unam/administracion/controller/MainViewController.java
+++ src/main/java/fes/aragon/unam/administracion/controller/MainViewController.java
@@ -1,13 +1,20 @@
 package fes.aragon.unam.administracion.controller;
 
+import fes.aragon.unam.administracion.model.*;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class MainViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
 
     @FXML
     private TableColumn<?, ?> acciones;
@@ -19,26 +26,38 @@ public class MainViewController {
     private Button btnAgregar;
 
     @FXML
-    private TableColumn<?, ?> cajas;
+    private TableColumn<?, ?> colAcciones;
 
     @FXML
-    private TableColumn<?, ?> conductor;
+    private TableColumn<Caja, String> colCajas;
 
     @FXML
-    private TableColumn<?, ?> id;
+    private TableColumn<Trabajador, String> colConductor;
 
     @FXML
-    private TableColumn<?, ?> matricula;
+    private TableColumn<Camion, String> colId;
 
     @FXML
-    private TableView<?> tabla;
+    private TableColumn<Camion, String> colMatricula;
 
     @FXML
-    private TableColumn<?, ?> zona;
+    private TableColumn<Zona, String> colZona;
 
     @FXML
     void agregaraTabla(ActionEvent event) {
 
     }
+    private ObservableList<Camion> datosTabla;
+    private GestorCamiones gestor;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        gestor= GestorCamiones.getInstance();
+
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colConductor.setCellValueFactory(new PropertyValueFactory<>("trabajador"));
+        colZona.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colMatricula.setCellValueFactory(new PropertyValueFactory<>("matricula"));
+
+    }
 }
--- src/main/java/fes/aragon/unam/administracion/model/Camion.java
+++ src/main/java/fes/aragon/unam/administracion/model/Camion.java
@@ -3,7 +3,8 @@ package fes.aragon.unam.administracion.model;
 import java.util.ArrayList;
 
 public class Camion {
-    private  String numPlaca;
+    private int id;
+    private  String matricula;
     private ArrayList<Caja> cajas = new ArrayList<>();
     private ArrayList<Zona> zonas = new ArrayList<>();
     private Trabajador trabajador;
