9b67e57f3aeeb7fe8eff99ad10b3482b2ef4ab25
dachu_dev <dachu_dev@MacBook-Air-de-dachu-dev.local>
2026-05-16 20:58:45 -0600

    Botones de seleccion

--- src/main/java/fes/aragon/unam/administracion/controller/MainController.java
+++ src/main/java/fes/aragon/unam/administracion/controller/MainController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -13,9 +14,33 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+    @FXML
+    private Button btnDashboard;
+
+    @FXML
+    private Button btnProductos;
+
+    @FXML
+    private Button btnSalir;
+
+    @FXML
+    private Button btnTrabajador;
+
+    @FXML
+    private Button btnZonas;
 
     @FXML
     private AnchorPane contenedor;
+    //Reinicia los botones permitiendo hacer el efecto de seleccion de botone
+    private void resetBotton(Button btnActual)
+    {
+        btnDashboard.setStyle("-fx-background-color: #313a46");
+        btnProductos.setStyle("-fx-background-color: #313a46");
+        btnSalir.setStyle("-fx-background-color: #313a46");
+        btnTrabajador.setStyle("-fx-background-color: #313a46");
+        btnZonas.setStyle("-fx-background-color: #313a46");
+        btnActual.setStyle("-fx-background-color: #4a90d9");
+    }
 
     private void cargarVista(String fxml) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
@@ -31,27 +56,32 @@ public class MainController implements Initializable {
 
     @FXML
     void dashboard(ActionEvent event) throws IOException {
-        // aquí puedes cargar una vista de dashboard cuando la tengas
+        resetBotton(btnDashboard);
         cargarVista("/fes/aragon/unam/administracion/main-view.fxml");
+
     }
     @FXML
     void irTrabajador(ActionEvent event) throws IOException {
+        resetBotton(btnTrabajador);
         cargarVista("/fes/aragon/unam/administracion/trabajador.fxml");
     }
 
 
     @FXML
     void productos(ActionEvent event) throws IOException {
+        resetBotton(btnProductos);
         cargarVista("/fes/aragon/unam/administracion/productos-view.fxml");
     }
     @FXML
     void zonas(ActionEvent event) throws IOException {
+        resetBotton(btnZonas);
         cargarVista("/fes/aragon/unam/administracion/zonas-view.fxml");
     }
 
 
     @FXML
     void salir(ActionEvent event) {
+        resetBotton(btnSalir);
         Stage stage = (Stage) contenedor.getScene().getWindow();
         stage.close();
     }
